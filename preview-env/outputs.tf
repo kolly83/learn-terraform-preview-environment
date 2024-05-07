49e7cc46799c32fbbd2f9b2129b416d5b1bf74c7
kolly83 <kolly22@gmail.com>
2024-05-07 17:06:47 +0100

    Adding lb_count to outputs.tf

--- preview-env/outputs.tf
+++ preview-env/outputs.tf
@@ -2,6 +2,11 @@ output "lb_dns_name" {
   value = length(aws_lb.app) > 0 ? "https://${aws_lb.app[0].dns_name}" : ""
 }
 
+output "lb_count" {
+  value = length(aws_lb.app)
+}
+
+
 output "public_ip" {
   value = length(aws_instance.hashicups-backend) > 0 ? "https://${aws_instance.hashicups-backend[0].public_ip}" : ""
 }
